d15e6073734f9fe0ccd45db12cb6c68c1fbc41d6
Harshpandey9156 <Harshpandey21012@gmail.com>
2023-12-19 12:11:15 +0530

    name letter

--- stl/namelettercount.cpp
+++ stl/namelettercount.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 int main()
 {
     //count the number of letter in each word using map
     string name;
     cin>>name;
+    unordered_map<char,int>mp;
+    for(auto chr:name){
+        mp[chr]++;
+
+    }
+    for(auto i:mp){
+        cout<<i.first<<"  "<<i.second;
+    }
 
 }
