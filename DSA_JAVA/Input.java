17b825ce8fa6bee01612dfef849ad5a41f58daab
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-06 16:33:39 +0530

    BASic in JAva

---
+++ DSA_JAVA/Input.java
@@ -0,0 +1,8 @@
+package DSA_JAVA;
+
+public class Input {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ DSA_JAVA/Main.java
@@ -0,0 +1,14 @@
+package DSA_JAVA;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        // System.out.println(args[0]);
+        Scanner input= new Scanner(System.in);
+        System.out.println(input.nextInt());
+
+
+
+    }
+}
