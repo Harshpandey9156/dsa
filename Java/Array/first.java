9f8e1e24aa45ef0687ad38f73adc403dc1384457
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-08 17:59:15 +0530

    array

--- Java/Array/first.java
+++ Java/Array/first.java
@@ -1,6 +1,15 @@
 package Java.Array;
 
 public class first {
+    public static void main(String[] args) {
+        int[] arr; //declaration of the array 
+        int[]  arrr=new int[8]; // cretaing the object in the heap memory 
+        System.out.println(arrr[2]);
+
+        String[] str;
+        String[]  str1=new String[10];
+        System.out.println(str1[2]);
+    }
     
     
 }
