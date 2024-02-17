3dc6970c0cb1e238cd13189160429824a76ada42
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-17 13:56:41 +0530

    Searching in 2D

--- Java/Array/SearchingIN2D.java
+++ Java/Array/SearchingIN2D.java
@@ -1,7 +1,35 @@
 package Java.Array;
 
+import java.util.Arrays;
+
 public class SearchingIN2D {
     public static void main(String[] args) {
+        int[][] arr={
+            {1,3,4,5},
+            {1,44,12,11,3},
+            {3,5,7,78}
+        };
+        int target =12;
+        int[] ans=Search(arr, target) ;
+        System.out.println(Arrays.toString(ans));
+        
+    }
+    static int[] Search(int[][] arr,int target){
+        // int ans[]  ;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j]==target){
+                    return new int[]{i,j};
+                }
+
+                
+            }
+
+            
+            
+        }
+        return new int[]{-1,-1};
+
         
     }
 }
