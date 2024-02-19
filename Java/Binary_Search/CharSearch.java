173dd414a2e7aafddffc842b7596c006418b248d
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-19 18:59:11 +0530

    First and last

--- Java/Binary_Search/CharSearch.java
+++ Java/Binary_Search/CharSearch.java
@@ -14,6 +14,7 @@ public class CharSearch {
                         }
                         else{
                             end=mid-1;
+                            
                         }
                     
                 }
