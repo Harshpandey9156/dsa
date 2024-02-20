fb2073087f99996a0ac80edea956fe3e295cbb0b
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-20 21:36:19 +0530

    binary updation

--- Java/Binary_Search/FirstAndLast.java
+++ Java/Binary_Search/FirstAndLast.java
@@ -9,7 +9,9 @@ public class FirstAndLast {
 
     static int[] searchRange(int[] arr, int target) {
         int[] ans={-1,-1};
+        return ans;
     }
+
     //return the function which we are calling it twice for left and the write 
     int search(int[] arr ,int target ,boolean FirstStartIndex){
         int ans=-1;
