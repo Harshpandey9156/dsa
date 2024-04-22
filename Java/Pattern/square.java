b8e2bca697500a6d2e02d67ae975118d57fd6622
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-04-22 17:42:13 +0530

    Patterns and String

---
+++ Java/Pattern/square.java
@@ -0,0 +1,23 @@
+package Java.Pattern;
+
+public class square {
+    public static void main(String[] args) {
+        pattern1(5);
+        
+        
+        
+    }
+    static void pattern1(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+ " " );
+            }
+            System.out.println();
+        }
+    }
+    static void pattern3(int n ){
+        // for
+    }
+     
+    static void pattern5()
+}
